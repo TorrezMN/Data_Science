eacb1bc981e42a5c90c9c23d85ae9cf4c594501f
TorrezMN <torrez.mn@gmail.com>
2023-01-02 16:54:47 -0300

    first commit

--- Enc_Devs_Py/Notebooks/toolkit/tools.py
+++ Enc_Devs_Py/Notebooks/toolkit/tools.py
@@ -1,22 +1,10 @@
 import itertools
 import pandas as pd
+from collections import Counter
+
+# CLASES
+df_class = pd.core.frame.DataFrame
 
 
-def print_column_uniques(df,col):
-    """ Print each of the UNIQUE values in a column where it is needed. Like so:
-        ROW -> item 1; item 2; item 2, item 3;
-        will print this:
-        ite 1, item 2, item 3
-    """
-        
-    print(set(itertools.chain.from_iterable([i.split(';') for i in df[col]])))
-    
-    
-def make_df(df, col):
-    """ Returns a dataframe from column values."""
-    c = set(itertools.chain.from_iterable([i.split(';') for i in df[col].value_counts().keys()]))
-    cats = {i:0 for i in c}
-    for i in c:
-        cats[i] = df[df[col].str.contains(i)].shape[0]
-    df = pd.DataFrame(data = [i for i in cats.items()], columns=['cat', 'count']).set_index('cat')
-    return(df)
+def get_column_uniques(df: df_class, col: str):
+    return list(set(itertools.chain.from_iterable([i.split(";") for i in df[col]])))
