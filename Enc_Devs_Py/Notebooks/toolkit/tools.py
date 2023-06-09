e8e69b8e101f1d27fc8c6155b50e7492359cf60c
TorrezMN <torrez.mn@gmail.com>
2023-06-09 17:32:52 -0400

    ADDED some changes AND modifications to 'salaries' notebook.

--- Enc_Devs_Py/Notebooks/toolkit/tools.py
+++ Enc_Devs_Py/Notebooks/toolkit/tools.py
@@ -2,6 +2,15 @@ import itertools
 import pandas as pd
 
 
+def get_column_uniques(df, col):
+    """Print each of the UNIQUE values in a column where it is needed. Like so:
+    ROW -> item 1; item 2; item 2, item 3;
+    will print this:
+    ite 1, item 2, item 3
+    """
+
+    return(list(set(itertools.chain.from_iterable([i.split(";") for i in df[col]]))))
+
 def print_column_uniques(df, col):
     """Print each of the UNIQUE values in a column where it is needed. Like so:
     ROW -> item 1; item 2; item 2, item 3;
