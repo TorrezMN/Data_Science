bd5139d40bef3b5ec78dba568b93cb0472e342e7
TorrezMN <torrez.mn@gmail.com>
2023-06-14 16:22:42 -0400

    REFACTORED 'education' notebook AND lint formated all notebooks.

--- Enc_Devs_Py/Notebooks/toolkit/tools.py
+++ Enc_Devs_Py/Notebooks/toolkit/tools.py
@@ -9,7 +9,8 @@ def get_column_uniques(df, col):
     ite 1, item 2, item 3
     """
 
-    return(list(set(itertools.chain.from_iterable([i.split(";") for i in df[col]]))))
+    return list(set(itertools.chain.from_iterable([i.split(";") for i in df[col]])))
+
 
 def print_column_uniques(df, col):
     """Print each of the UNIQUE values in a column where it is needed. Like so:
@@ -38,7 +39,8 @@ def make_df(df, col, x_label, y_label):
     for i in c:
         cats[i] = df[df[col].str.contains(i)].shape[0]
     df = pd.DataFrame(
-        data=[i for i in cats.items()], columns=[x_label.replace(' ', ''), y_label.replace(' ', '')]
-    )#.set_index(x_label.replace(' ',''))
+        data=[i for i in cats.items()],
+        columns=[x_label.replace(" ", ""), y_label.replace(" ", "")],
+    )  # .set_index(x_label.replace(' ',''))
 
     return df
