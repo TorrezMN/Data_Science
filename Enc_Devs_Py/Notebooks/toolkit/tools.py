f38a6dcf9753bc07743a1adcf1640f07e65dd3f8
TorrezMN <torrez.mn@gmail.com>
2023-06-21 22:27:32 -0400

    FINISH 'gender' notebook.

--- Enc_Devs_Py/Notebooks/toolkit/tools.py
+++ Enc_Devs_Py/Notebooks/toolkit/tools.py
@@ -3,10 +3,43 @@ import pandas as pd
 import random
 
 
+def get_color(g, t):
+    # Light
+    color_hombres_light = (12 / 255, 50 / 255, 196 / 255, 0.5)
+    color_mujeres_light = (255 / 255, 192 / 255, 203 / 255, 0.5)
+    color_neutro_light = (149 / 255, 165 / 255, 166 / 255, 0.5)
+    # Dark
+    color_hombres_dark = (12 / 255, 50 / 255, 196 / 255, 0.8)
+    color_mujeres_dark = (255 / 255, 192 / 255, 203 / 255, 0.8)
+    color_neutro_dark = (149 / 255, 165 / 255, 166 / 255, 0.8)
+    # NONE COLOR
+    color_none = (49 / 255, 15 / 255, 6 / 255, 0.8)
+
+    cl = {
+        "Male": color_hombres_light,
+        "Female": color_mujeres_light,
+        "I do not share.": color_neutro_light,
+    }
+    cd = {
+        "Male": color_hombres_dark,
+        "Female": color_mujeres_dark,
+        "I do not share.": color_neutro_dark,
+    }
+
+    if t == "light":
+        return cl.get(g, color_none)
+    else:
+        return cd.get(g, color_none)
+
+
+def percentage_to_normal(df):
+    return df.mul(100).round(1).astype(str) + " %"
+
+
 def explode_pie(exp_range):
     """Returns a list of values to explode pie chart."""
-    return [random.uniform(0.01,0.04) for i in range(0,exp_range)]
-    
+    exp = [random.uniform(0.01, 0.05) for i in range(0, exp_range)]
+    return exp
 
 
 def get_column_uniques(df, col):
